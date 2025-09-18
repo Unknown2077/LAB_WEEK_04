8c5f093ccb278a129a7641b035aff33c44a0b059
Muhammad Aryasatya Triputra <m.aryasatya.triputra@gmail.com>
2025-09-18 20:53:50 +0700

    Commit No. 4 – Assignment

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -1,19 +1,17 @@
 package com.example.lab_week_04
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
-    FragmentStateAdapter(fragmentManager, lifecycle) {
+class CafeAdapter(private val hostFragment: Fragment) :
+    FragmentStateAdapter(hostFragment) {
 
-    constructor(fragment: Fragment) : this(fragment.childFragmentManager, fragment.lifecycle)
     override fun getItemCount(): Int {
         return TABS_FIXED.size
     }
 
     override fun createFragment(position: Int): Fragment {
-        return CafeDetailFragment()
+        val content = hostFragment.getString(TAB_CONTENTS[position])
+        return CafeDetailFragment.newInstance(content)
     }
 }
